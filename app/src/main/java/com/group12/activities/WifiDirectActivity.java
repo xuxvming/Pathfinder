73da6eef45f5e7993137f7ece0e026b3bb0b2cbe
xuxvming <oliverxiu18@gmail.com>
2020-02-12 12:17:57 +0000

    rename files

--- app/src/main/java/com/group12/activities/WifiDirectActivity.java
+++ app/src/main/java/com/group12/activities/WifiDirectActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2011 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.group12.activities;
 
@@ -54,7 +39,7 @@ import com.group12.p2p.WiFiDirectBroadcastReceiver;
  */
 public class WiFiDirectActivity extends AppCompatActivity implements ChannelListener, DeviceListFragment.DeviceActionListener {
 
-    public static final String TAG = "wifidirectdemo";
+    public static final String TAG = "WiFiDirectActivity";
 
     private static final int PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION = 1001;
 
@@ -89,7 +74,7 @@ public class WiFiDirectActivity extends AppCompatActivity implements ChannelList
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_p2p);
 
         // add necessary intent values to be matched.
 
