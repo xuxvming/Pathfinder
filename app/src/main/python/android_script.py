276a4163b15feb63cc827c8bb8ee70b2cb76c070
hughesp8 <hughesp7@tcd.ie>
2020-04-26 18:51:12 +0100

    Added tests and routed test graph to assets graph.json

--- app/src/main/python/android_script.py
+++ app/src/main/python/android_script.py
@@ -56,30 +56,7 @@ def valid_edge(edge_dict, mode):
             return True
     return False
 
-def _weight_function(G, weight):
-    """Returns a function that returns the weight of an edge.
-    The returned function is specifically suitable for input to
-    functions :func:`_dijkstra` and :func:`_bellman_ford_relaxation`.
-    Parameters
-    ----------
-    G : NetworkX graph.
-    weight : string or function
-        If it is callable, `weight` itself is returned. If it is a string,
-        it is assumed to be the name of the edge attribute that represents
-        the weight of an edge. In that case, a function is returned that
-        gets the edge weight according to the specified edge attribute.
-    Returns
-    -------
-    function
-        This function returns a callable that accepts exactly three inputs:
-        a node, an node adjacent to the first one, and the edge attribute
-        dictionary for the eedge joining those nodes. That function returns
-        a number representing the weight of an edge.
-    If `G` is a multigraph, and `weight` is not callable, the
-    minimum edge weight over all parallel edges is returned. If any edge
-    does not have an attribute with key `weight`, it is assumed to
-    have weight one.
-    """
+def weight_function(G, weight):
     if callable(weight):
         return weight
     # If the weight keyword argument is not callable, we assume it is a
@@ -90,45 +67,7 @@ def _weight_function(G, weight):
         return x
     return lambda u, v, data: data.get(weight, 1)
 
-def _dijkstra_multisource(G, sources, weight, pred=None, paths=None,
-                          cutoff=None, target=None, mode='drive'):
-    """Uses Dijkstra's algorithm to find shortest weighted paths
-    Parameters
-    ----------
-    G : NetworkX graph
-    sources : non-empty iterable of nodes
-        Starting nodes for paths. If this is just an iterable containing
-        a single node, then all paths computed by this function will
-        start from that node. If there are two or more nodes in this
-        iterable, the computed paths may begin from any one of the start
-        nodes.
-    weight: function
-        Function with (u, v, data) input that returns that edges weight
-    pred: dict of lists, optional(default=None)
-        dict to store a list of predecessors keyed by that node
-        If None, predecessors are not stored.
-    paths: dict, optional (default=None)
-        dict to store the path list from source to each node, keyed by node.
-        If None, paths are not stored.
-    target : node label, optional
-        Ending node for path. Search is halted when target is found.
-    cutoff : integer or float, optional
-        Depth to stop the search. Only return paths with length <= cutoff.
-    Returns
-    -------
-    distance : dictionary
-        A mapping from node to shortest distance to that node from one
-        of the source nodes.
-    Raises
-    ------
-    NodeNotFound
-        If any of `sources` is not in `G`.
-    Notes
-    -----
-    The optional predecessor and path dictionaries can be accessed by
-    the caller through the original pred and paths objects passed
-    as arguments. No need to explicitly return pred or paths.
-    """
+def dijkstra(G, sources, weight, pred=None, paths=None, cutoff=None, target=None, mode='drive'):
     G_succ = G._succ if G.is_directed() else G._adj
 
     push = heappush
@@ -138,7 +77,6 @@ def _dijkstra_multisource(G, sources, weight, pred=None, paths=None,
     # fringe is heapq with 3-tuples (distance,c,node)
     # use the count c to avoid comparing nodes (may not be able to)
     from itertools import count
-    #pdb.set_trace()
     c = count()
     fringe = []
     for source in sources:
@@ -191,10 +129,9 @@ def shortest_path(G, sources=None, target=None, cutoff=None, weight=None, mode='
         raise ValueError('source must not be empty')
     if target in sources:
         return {'length': 0, 'nodes': [target]}
-    weight = _weight_function(G, weight)
+    weight = weight_function(G, weight)
     paths = {source: [source] for source in sources}  # dictionary of paths
-    dist = _dijkstra_multisource(G, sources, weight, paths=paths,
-                                 cutoff=cutoff, target=target, mode=mode)
+    dist = dijkstra(G, sources, weight, paths=paths, cutoff=cutoff, target=target, mode=mode)
     if target is None:
         return {'length': dist, 'nodes': [paths]}
     try:
